e20bdd130964b492364d86dbad28ec94164e6714
a1l2v <alvinst2005@gmail.com>
2025-07-16 21:10:12 +0530

    callack route coorected

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from 'next/server'
-// The client you created from the Server-Side Auth instructions
 import { createClientForServer } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
   let next = searchParams.get('next') ?? '/'
   if (!next.startsWith('/')) {
-    // if "next" is not a relative URL, use the default
     next = '/'
   }
 
@@ -16,19 +13,14 @@ export async function GET(request: Request) {
     const supabase = await createClientForServer()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      // Fix starts here
+      const host = request.headers.get('x-forwarded-host') || request.headers.get('host')
+      const protocol = host?.includes('localhost') ? 'http' : 'https'
+      const baseUrl = `${protocol}://${host}`
+
+      return NextResponse.redirect(`${baseUrl}${next}`)
     }
   }
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect('/')
 }
